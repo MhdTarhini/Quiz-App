aea0a62c246fb0dac0f1e45b1a1ab37f1616104b
Mohamad Tarhini <mohamedtahini15@gmail.com>
2023-04-16 04:06:19 +0300

    update

--- main.js
+++ main.js
@@ -2,47 +2,40 @@ let countSpan = document.querySelector(".count span.all-question-num"); //select
 btnStart = document.querySelector(".Start"); //select start button
 let quizArea = document.querySelector(".quiz-area"); //select div of quiz area
 let allAnswerArea = document.querySelector(".answers-area"); //select div of answers area
+
 function getQuestions() {
   //get question api
   let myRequest = new XMLHttpRequest();
   myRequest.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       let questionObject = JSON.parse(this.responseText);
-      let questionCount = questionObject.results.length;
+
       randomQues(questionObject.results); // display random question
-      allQuesNum(arrayQues.length); // update question counter
-      let theNum = -1;
-      //   console.log(arrayQues);
+      countSpan.innerHTML = 10; // every quiz is 10 question
+      let theNum = 1; // question counter
+      let correct_answer = arrayQues.pop(); // pop the correct answers into a new array
+
       function showFirstQues() {
-        answerResult(theNum, arrayQues); // check answers
+        answerResult(correct_answer); // check answers
         removeSelect(); //remove radio check
-        theNum++; // update variable theNum
-        if (theNum < 10) {
-          let quesNum = document.querySelector(".quiz-info .count span");
-          quesNum.innerHTML = `${theNum + 1}`;
-        }
-        let firstH2 = document.querySelector(".quiz-area h2");
-        firstH2.innerHTML = arrayQues[0];
-        arrayQues.shift();
-        console.log("!!11");
-        console.log(arrayQues);
+
+        let questionH2 = document.querySelector(".quiz-area h2");
+        questionH2.innerHTML = arrayQues[0]; //display question into h2
+        arrayQues.shift(); // remove the question already displayed from the array
+
         for (let i = 0; i < 4; i++) {
-          answer = document.querySelector(`#answer-${i + 1}`);
-          //   if (i <= 2) {
-          //     textAnswer = arrayQues[theNum].incorrect_answers[i];
-          //   } else {
-          //     textAnswer = arrayQues[theNum].correct_answer;
-          //   }
-          answer.innerHTML = arrayQues[0];
-          arrayQues.shift();
+          answer = document.querySelector(`#answer-${i + 1}`); // get radio input by specific id
+          answer.innerHTML = arrayQues[0]; // display answer
+          arrayQues.shift(); // remove every answer already displayed from the array
         }
-        btnStart.classList.add("disable");
-        quizArea.classList.remove("disable");
-        allAnswerArea.classList.remove("disable");
-        if (theNum <= arrayQues.length - 2) {
+        btnStart.classList.add("disable"); // disable the start button
+        quizArea.classList.remove("disable"); // show the quiz area
+        allAnswerArea.classList.remove("disable"); // show answers area
+
+        if (theNum <= 10) {
           btn.classList.remove("disable"); // if the quiz is not finish the submit button will still display
         } else {
-          //   console.log();
+          theNum = 10;
           btnStart.classList.remove("disable"); // show start btn and transfer to result button
           btnStart.innerHTML = `Your result is ${pointResult}/10`; // show results
           //results grades
@@ -58,10 +51,13 @@ function getQuestions() {
           quizArea.classList.add("disable"); // disable quiz area
           allAnswerArea.classList.add("disable"); // disable answers area
         }
+        let quesNum = document.querySelector(".quiz-info .count span");
+        // update variable theNum
+        quesNum.innerHTML = `${theNum++}`;
       }
-      btnStart.onclick = showFirstQues;
+      btnStart.onclick = showFirstQues; // start quiz
       let btn = document.querySelector(".submit-button.submit");
-      btn.onclick = showFirstQues;
+      btn.onclick = showFirstQues; // re-call the function after submit the answer
     }
   };
   myRequest.open(
@@ -73,41 +69,41 @@ function getQuestions() {
 }
 getQuestions();
 
-function allQuesNum(num) {
-  countSpan.innerHTML = `${num}`;
-}
-
-function randomQues(question) {
-  arrayQues = [];
-  answers = [];
-  correct_answer = [];
+function randomQues(data) {
+  arrayQues = []; // define array for all data
+  answers = []; // for every question correct and incorrect answers
+  correct_answer = []; // correct answers only
   for (let i = 0; i < 10; i++) {
-    var randomElement = question[Math.floor(Math.random() * question.length)];
-    arrayQues.push(randomElement.question);
-    answers.push(randomElement.correct_answer);
-    correct_answer.push(randomElement.correct_answer);
+    var randomElement = data[Math.floor(Math.random() * data.length)]; // get random question
+    data = data.filter(
+      (element) => element.question !== randomElement.question // filter the data from the questions already used
+    );
+    arrayQues.push(randomElement.question); // push the question
+    answers.push(randomElement.correct_answer); // push the correct answers
+    correct_answer.push(randomElement.correct_answer); // push the correct answers
     for (let i = 0; i < 3; i++) {
-      answers.push(randomElement.incorrect_answers[i]);
+      answers.push(randomElement.incorrect_answers[i]); // push incorrect answers inside the array
     }
     for (let i = 0; i < 4; i++) {
-      var randomAnswers = answers[Math.floor(Math.random() * answers.length)];
-      arrayQues.push(randomAnswers);
-      answers = answers.filter((element) => element !== randomAnswers);
+      var randomAnswers = answers[Math.floor(Math.random() * answers.length)]; // get random answers (correct or incorrect)
+      arrayQues.push(randomAnswers); // push all answers inside the array
+      answers = answers.filter((element) => element !== randomAnswers); // remove the answer already pushed
     }
   }
-  arrayQues.push(correct_answer);
-  console.log(arrayQues[-1]);
+  arrayQues.push(correct_answer); // add correct_answers (all correct answers) in the end of array
   return arrayQues;
 }
 
-let pointResult = 1;
-function answerResult(theNum, arrayQues) {
+let pointResult = 0;
+function answerResult(correct_answer) {
   let answerArea = document.querySelectorAll(".answers-area input");
   answerArea.forEach((ele, index) => {
     if (ele.checked) {
+      // check if the answers in included inside the correct answers array
       if (
-        document.getElementById(`answer-${index + 1}`).innerHTML ==
-        arrayQues[theNum].correct_answer
+        correct_answer.includes(
+          document.getElementById(`answer-${index + 1}`).innerHTML
+        )
       ) {
         pointResult++;
       }
